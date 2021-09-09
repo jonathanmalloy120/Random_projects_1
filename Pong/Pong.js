f1351c0d04b179521e3da86ca41d4992878ded09
Jon Malloy <jonathan.malloy3@gmail.com>
2021-09-09 11:44:46 -0400

    added some paddle collision detection

--- Pong/Pong.js
+++ Pong/Pong.js
@@ -11,6 +11,7 @@ function createLeftPaddle() {
     let leftPaddle = document.createElement('div'); //create element
     leftPaddle.id = "paddleLeft"; //set id for element
     leftPaddle.style.left = 20 + 'px' //set location
+    leftPaddle.style.top = ((winHeight/2) - (paddleHeight/2)) + 'px'
     leftPaddle.speed = 0; //placeholder for speed later
     document.body.appendChild(leftPaddle); // push onto document
     paddles.push(leftPaddle) //add to array for later
@@ -20,6 +21,7 @@ function createRightPaddle() {
     let rightPaddle = document.createElement('div');
     rightPaddle.id = "paddleRight";
     rightPaddle.style.left = (winWidth - paddleWidth -20) +'px'; //put right paddle 20 px in from edge
+    rightPaddle.style.top = ((winHeight/2) - (paddleHeight/2)) + 'px'
     rightPaddle.speed = 0; //placeholder for speed later
     document.body.appendChild(rightPaddle);
     paddles.push(rightPaddle)
@@ -28,8 +30,8 @@ function createRightPaddle() {
 function createBall() {
     let ball = document.createElement('div');
     ball.id = "ball";
-    ball.dx = 5; //initialize x velocity
-    ball.dy = 0;//initialize y velocity
+    ball.dx = 10; //initialize x velocity
+    ball.dy = -2;//initialize y velocity
     //console.log(ball)
     ball.style.left = (winWidth/2) - (ballDiameter) + 'px'; // center ball
     ball.style.top = (winHeight/2) - (ballDiameter) + 'px'; //center ball
@@ -51,19 +53,30 @@ function moveball() {
         ball.style.top = (parseInt(ball.style.top) - ball.dy) + 'px';
     }
     
-    checkPaddleCollide()
     setTimeout(moveball,58); //moves ball
+    checkPaddleCollide()
+
 }
 
 function checkPaddleCollide() {
     let ball = document.getElementById('ball');
-    let leftPaddle = paddles[0];
-    let rightPaddle = paddles[1];
+    let leftPaddle = document.getElementById('paddleLeft');
+    let rightPaddle = document.getElementById('paddleRight');
+    console.log(rightPaddle);
+    console.log(leftPaddle);
+    let ballLeft = parseInt(ball.style.left);
+    let ballTop = parseInt(ball.style.top);
+    console.log(ballLeft, ballTop, parseInt(leftPaddle.style.top), parseInt(leftPaddle.style.top) + paddleHeight);
     //first check left paddle
-    if(parseInt(ball.style.left) < parseInt(leftPaddle.style.left) + paddleWidth) { //TODO: add up/down check to make sure we are within the paddle's top and bottom
+    if((ballLeft < parseInt(leftPaddle.style.left) + paddleWidth) && (ballTop + ballDiameter > parseInt(leftPaddle.style.top)) && (ballTop < parseInt(leftPaddle.style.top) + paddleHeight)) { //TODO: add up/down check to make sure we are within the paddle's top and bottom
         ballLeftRight = !ballLeftRight;
-    } else {
+        
+    } else if (ballLeft < parseInt(leftPaddle.style.left)) {
         //code for scoring here
+        console.log('missed!')
+
+    } else {
+
     }
     //check right paddle
     //TODO finish this
@@ -76,4 +89,4 @@ function checkPaddleCollide() {
 createLeftPaddle();
 createRightPaddle();
 createBall();
-//moveball();
+moveball();
